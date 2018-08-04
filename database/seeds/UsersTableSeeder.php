a2565814a2bf42d7ef34de4d8c034ee906c0339c
Ruruo <508170051@qq.com>
2018-08-04 12:19:44 +0800

    文章列表完成

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -37,10 +37,10 @@ class UsersTableSeeder extends Seeder
 		User::insert($user_array);
 
 		// 单独处理第一个用户的数据
-		$user         = User::find(1);
-		$user->name   = 'Ruruo';
-		$user->email  = 'hu508170051@gmail.com';
-		$user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+		$user           = User::find(1);
+		$user->name     = 'Ruruo';
+		$user->email    = 'hu508170051@gmail.com';
+		$user->avatar   = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
 		$user->save();
 
 	}
